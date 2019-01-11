19e17f76e820b44f433643121b0c9d3b28721242
Lucien Morey <lucienp72@gmail.com>
2019-01-11 12:19:27 +1100

    file clean up. bug where only runs correctly in pycharm interpretter

--- AudioStream.py
+++ AudioStream.py
@@ -29,58 +29,21 @@ class AudioStream(object):
             output=True,
             frames_per_buffer=self.CHUNK,
         )
-       # self.init_plots()
-
-    def init_plots(self):
-        # x variables for plotting
-        x = np.arange(0, 2 * self.CHUNK, 2)
-        xf = np.linspace(0, self.RATE, self.CHUNK)
-
-        # create matplotlib figure and axes
-        self.fig, (ax1, ax2) = plt.subplots(2, figsize=(15, 7))
-
-
-        # create a line object with random data
-        self.line, = ax1.plot(x, np.random.rand(self.CHUNK), '-', lw=2)
-
-        
-        # format waveform axes
-        ax1.set_title('AUDIO WAVEFORM')
-        ax1.set_xlabel('samples')
-        ax1.set_ylabel('volume')
-
-        plt.setp(
-            ax1, yticks=[0, 128, 255],
-            xticks=[0, self.CHUNK, 2 * self.CHUNK],
-        )
-        plt.setp(ax2, yticks=[0, 1], )
-
-        # format spectrum axes
-        ax2.set_xlim(20, self.RATE / 2)
-
-
-
 
 
 if __name__ == '__main__':
     mystream = AudioStream()
-
-
     fig, ax = plt.subplots()
     x = np.arange(0, 2 * mystream.CHUNK, 2)
     line, = ax.plot(x, np.random.rand(mystream.CHUNK))
     ax.set_ylim(0, 255)
     ax.set_xlim(0, 2 * mystream.CHUNK)
+    fig.canvas.draw()
     plt.show(block=False)
     while True:
         data = mystream.stream.read(mystream.CHUNK)
         data_int = np.array(struct.unpack(str(2 * mystream.CHUNK) + 'B', data), dtype='b')[::2] + 128
         line.set_ydata(data_int)
-        fig.canvas.draw()
+        ax.draw_artist(ax.patch)
+        ax.draw_artist(line)
         fig.canvas.flush_events()
-
-
-
-
-
-
