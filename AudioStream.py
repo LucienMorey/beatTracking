2cff116c46a9ab70ee8a1bddf8000006a442365d
LucienMorey <lucienp@live.com.au>
2019-01-12 21:06:13 +1100

    stream using madmom library

--- AudioStream.py
+++ AudioStream.py
@@ -7,6 +7,9 @@ import struct
 from scipy.fftpack import fft
 import sys
 import time
+import madmom
+
+from madmom.audio.signal import Stream
 
 
 class AudioStream(object):
@@ -32,18 +35,21 @@ class AudioStream(object):
 
 
 if __name__ == '__main__':
-    mystream = AudioStream()
-    fig, ax = plt.subplots()
-    x = np.arange(0, 2 * mystream.CHUNK, 2)
-    line, = ax.plot(x, np.random.rand(mystream.CHUNK))
-    ax.set_ylim(0, 255)
-    ax.set_xlim(0, 2 * mystream.CHUNK)
-    fig.canvas.draw()
-    plt.show(block=False)
+    stream = Stream(sample_rate=44100,
+                    num_channels=1,
+                    frame_size=1024*2
+                    )
+    proc = madmom.audio.signal.FramedSignalProcessor(origin='stream')
+    data = stream.next()
+    fs = proc.process(data)
+    spec = madmom.audio.spectrogram.Spectrogram(fs, frame_size=2048, hop_size=200, fft_size=4096)
+
+    plt.imshow(spec[:, :200].T, aspect='auto', origin='lower')
+
+    plt.show()
+
     while True:
-        data = mystream.stream.read(mystream.CHUNK)
-        data_int = np.array(struct.unpack(str(2 * mystream.CHUNK) + 'B', data), dtype='b')[::2] + 128
-        line.set_ydata(data_int)
-        ax.draw_artist(ax.patch)
-        ax.draw_artist(line)
-        fig.canvas.flush_events()
+        spec = madmom.audio.spectrogram.Spectrogram(fs, frame_size=2048, hop_size=200, fft_size=4096)
+
+        plt.imshow(spec[:, :200].T, aspect='auto', origin='lower')
+
