82c2f9cd547c64bbb49627f10cdefa156c46e523
Lucien Morey <lucienp72@gmail.com>
2019-01-13 00:24:30 +1100

    suspected live stream with superflux filter,need to live display data to make sure

--- AudioStream.py
+++ AudioStream.py
@@ -4,7 +4,7 @@ import pyaudio
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import struct
-from scipy.fftpack import fft
+from scipy.ndimage.filters import maximum_filter
 import sys
 import time
 import madmom
@@ -12,44 +12,29 @@ import madmom
 from madmom.audio.signal import Stream
 
 
-class AudioStream(object):
-    def __init__(self):
-
-        # stream constants
-        self.CHUNK = 1024 * 2
-        self.FORMAT = pyaudio.paInt16
-        self.CHANNELS = 1
-        self.RATE = 44100
-        self.pause = False
-
-        # stream object
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format=self.FORMAT,
-            channels=self.CHANNELS,
-            rate=self.RATE,
-            input=True,
-            output=True,
-            frames_per_buffer=self.CHUNK,
-        )
 
 
-if __name__ == '__main__':
-    stream = Stream(sample_rate=44100,
-                    num_channels=1,
-                    frame_size=1024*2
-                    )
-    proc = madmom.audio.signal.FramedSignalProcessor(origin='stream')
+
+stream = Stream(sample_rate=44100,
+                num_channels=1,
+                frame_size=1024*2
+                )
+proc = madmom.audio.signal.FramedSignalProcessor(origin='stream')
+while True:
     data = stream.next()
     fs = proc.process(data)
     spec = madmom.audio.spectrogram.Spectrogram(fs, frame_size=2048, hop_size=200, fft_size=4096)
+    filt_spec = madmom.audio.spectrogram.FilteredSpectrogram(spec,
+                                                             filterbank=madmom.audio.filters.LogFilterbank,
+                                                             num_bands=24)
+    log_spec = madmom.audio.spectrogram.LogarithmicSpectrogram(filt_spec, add=1)
+    size = (1, 3)
+    max_spec = maximum_filter(log_spec, size=size)
+    diff = np.zeros_like(log_spec)
+    diff[1:] = (log_spec[1:] - max_spec[: -1])
+    pos_diff = np.maximum(0, diff)
+    superflux = np.sum(pos_diff, axis=1)
 
-    plt.imshow(spec[:, :200].T, aspect='auto', origin='lower')
+    print(superflux)
 
-    plt.show()
-
-    while True:
-        spec = madmom.audio.spectrogram.Spectrogram(fs, frame_size=2048, hop_size=200, fft_size=4096)
-
-        plt.imshow(spec[:, :200].T, aspect='auto', origin='lower')
 
